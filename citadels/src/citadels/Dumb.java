12098d512ad6116b2d148afb52ec1e9dc01bde62
tangxin1998 <61822067+tangxin1998@users.noreply.github.com>
2020-05-06 19:04:27 +0800

    Add files via upload

--- citadels/src/citadels/Dumb.java
+++ citadels/src/citadels/Dumb.java
@@ -4,22 +4,44 @@ import java.util.List;
 import java.util.Random;
 
 public class Dumb extends Player{
-
+//  Dumb 玩家：有钱有卡就造. 不分颜色，不看身份，不算总分，不用技能，佛系玩家
     Dumb(Personnas.Personnage personage) {
         super(personage);
     }
     @Override
-    void action(List<Quartiers.Quartier> quartiers){
-        if(quartiers.size()>0) {
-            if (countquartier()<7) {
-                for(Quartiers.Quartier m : quartierenmain){
-                    if(argent>=m.price){
-                        get1Quartiers(quartiers);
-                        return;
-                    }
+    void action(List<Quartiers.Quartier> quartiers, List<Player> otherPlayer){
+
+        //1. 获取场上信息
+        countpoints();
+        int crntPoint = points; //分数
+        int crntMoney = argent; //金币
+        int crntHandCards = quartierenmain.size(); //建筑手牌数
+        int crntBuildings = quartierconstruit.size(); //已有建筑数
+
+        //2. 抽卡或金币阶段:
+        if (quartiers.size() > 0) {
+            if (countquartier() < 7) {
+                //有图书馆：但是选择要钱
+                if (quartierconstruit.contains(Quartiers.Quartier.Bibliotheque)) {
+                    addargent();
+                }else{
+                    addargent();
                 }
-                addargent();
             }
-        }else {addargent();}
+        } else {
+            //如果建筑卡组已用完，则只加钱
+            addargent();
+        }
+
+        //3. 建筑技能阶段:
+
+        //如果有实验室：统一不用技能
+        if (quartierconstruit.contains(Quartiers.Quartier.Laboratoire)) {
+//            System.out.println("Dumb 玩家拥有实验室");
+        }
+
+        //4. 建造阶段： (你们这个建造是写死了我来不及改了)
+        construitquartier();
+
     }
 }
