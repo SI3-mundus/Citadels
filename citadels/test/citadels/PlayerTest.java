fd7b1da81ef16caa4045cb2e2998a2887f57ab68
keyanfang <61821753+keyanfang@users.noreply.github.com>
2020-05-28 00:22:22 +0800

    加了些test 还在继续写

--- citadels/test/citadels/PlayerTest.java
+++ citadels/test/citadels/PlayerTest.java
@@ -2,16 +2,24 @@ package citadels;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class  PlayerTest {
     private static final Dumb dumb1= new Dumb(Personnas.Personnage.Assassin);
     private static final Dumb dumb2= new Dumb(Personnas.Personnage.Roi);
     private static final Smart smart1= new Smart(Personnas.Personnage.Magicien);
     private static final Smart smart2= new Smart(Personnas.Personnage.Marchand);
-    private static final Quartiers.Quartier quartier1= Quartiers.Quartier.Taverne;
-    private static final Quartiers.Quartier quartier2= Quartiers.Quartier.Marche;
-    private static final Quartiers.Quartier quartier3= Quartiers.Quartier.Caserne;
-    private static final Quartiers.Quartier quartier4= Quartiers.Quartier.Manufacture;
+    private static final Quartiers.Quartier taverne= Quartiers.Quartier.Taverne;
+    private static final Quartiers.Quartier marche= Quartiers.Quartier.Marche;
+    private static final Quartiers.Quartier caserne= Quartiers.Quartier.Caserne;
+    private static final Quartiers.Quartier manufacture= Quartiers.Quartier.Manufacture;
+    private static final Quartiers.Quartier prison = Quartiers.Quartier.Prison;
+    private static final Quartiers.Quartier eglise = Quartiers.Quartier.Eglise;
+    private static final Quartiers.Quartier palais = Quartiers.Quartier.Palais;
+    private static final Quartiers.Quartier chateau = Quartiers.Quartier.Chateau;
+    private static final Quartiers.Quartier cathedrale = Quartiers.Quartier.Cathedrale;
     private Quartiers q;
 
     @Test
@@ -26,12 +34,10 @@ public class  PlayerTest {
 
     @Test
     void action(){
-        dumb1.countpoints();
-        dumb2.countpoints();
-        smart1.countpoints();
-        smart2.countpoints();
         assertTrue(dumb1.points == 0);
-        assertTrue(smart1.points == 0);
+        assertTrue(smart1.currentpoints == 0);
+        dumb1.quartierenmain.add(marche);
+        assertTrue(dumb1.countPotentialPoints()==2);
     }
     @Test
     void addArgent(){
@@ -41,7 +47,9 @@ public class  PlayerTest {
     }
     @Test
     void countQuartier(){
-        assertTrue(smart1.countquartier()==0);
+        smart1.quartierconstruit.add(chateau);
+        smart1.quartierenmain.add(prison);
+        assertTrue(smart1.countquartier()==2);
     }
     @Test
     void get1Quartier(){
@@ -58,30 +66,33 @@ public class  PlayerTest {
     @Test
     void labAbility(){
         q=new Quartiers();
-        dumb1.quartierenmain=q.get4quartiers();
-        dumb1.laboratoire_ability(dumb1.quartierenmain,dumb1.lowestPriceCardIndex(dumb1.quartierenmain));
-        assertTrue(dumb1.argent==3);
+        smart1.quartierenmain=q.get4quartiers();
+        smart1.laboratoire_ability(smart1.quartierenmain,0);
+        assertTrue(smart1.argent==3);
     }
     @Test
     void countPotentialPoints(){
-        q=new Quartiers();
-        dumb1.quartierenmain=q.get4quartiers();
-        assertTrue(dumb1.countPotentialPoints()!=0);
+        dumb1.addargent();
+        dumb1.quartierenmain.add(eglise);
+        assertTrue(dumb1.countPotentialPoints()==2);
     }
     @Test
     void construitQuartier(){
-        smart1.quartierenmain.add(quartier1);
+        smart1.quartierenmain.add(taverne);
         smart1.construitquartier();
         assertTrue(smart1.quartierconstruit.size()==1);
-        smart2.quartierenmain.add(quartier4);
+        smart2.quartierenmain.add(manufacture);
         smart2.construitquartier();
         assertTrue(smart2.quartierconstruit.size()==0);
     }
     @Test
     void avaliableQuartier(){
-        q = new Quartiers();
-        smart2.quartierenmain = q.get4quartiers();
-        assertTrue(smart2.availablenNumberofquartiersenmain()==4);
+        smart2.quartierenmain.add(cathedrale);
+        smart2.quartierenmain.add(palais);
+        assertTrue(smart2.availablenNumberofquartiersenmain()==2);
+        smart2.quartierconstruit.add(cathedrale);
+        assertTrue(smart2.availablenNumberofquartiersenmain()==1);
     }
+
 
 }
